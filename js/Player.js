ee0a1dc533bf754599643d351fa78d038149ab1d
rooshaan <65545298+rooshaan@users.noreply.github.com>
2020-07-21 15:51:50 +0530

    Add files via upload

---
+++ js/Player.js
@@ -0,0 +1,20 @@
+class Player{
+  constructor()  {}
+    getCount(){
+        var playerCountRef=database.ref('playerCount');
+        playerCountRef.on("value",function(data){
+            playerCount=data.val();
+        })
+    }
+    update(name){
+        var playerIndex="player"+playerCount
+        database.ref('/').set({
+
+          name  :name        })
+
+    }
+    updateCount(count){
+        database.ref('/').update({
+            playerCount :count        })
+    }
+}
